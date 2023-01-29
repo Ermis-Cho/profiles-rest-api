ade7a10320002581689999c32b1556f95185348b
Ermis <ermis.chorinopoulos@gmail.com>
2023-01-30 00:01:53 +0100

    feat: Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
+##Used for overriding or customizing the default Django user model officially
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("Users must have an email address")
+        
+        email.self.normalize_email(email) #Converts in lowercase the 2nd half of an email
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, name, password)
+
+        user.is_super_user = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    #model manager for the objects
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+
+    #because we are defining a function in a class,
+    # we must specify self as the first argument this is the default Python convention
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+    
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of user"""
+        return self.email
