ed949872db0d01b5be21b7e9602942410c67fd16
Ermis <ermis.chorinopoulos@gmail.com>
2023-02-27 22:55:07 +0100

    feat[urls.py/views.py]: Added viewsets to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+router = DefaultRouter()
+# first "hello-viewset" is the name of the name of the url, we wish to ccreate
+# base_name="hello-viewset" for retrieving URLs in our router, if we ever need
+# to do that using the URL retrieving function providewd by dhango
+router.register("hello-viewset", views.HelloViewSet, base_name="hello-viewset")
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path("", include(router.urls))
 ]
