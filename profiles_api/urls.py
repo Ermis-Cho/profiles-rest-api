10e294d531a1227dfaf8ca8717ac99ab987e2263
Ermis <ermis.chorinopoulos@gmail.com>
2023-02-28 20:52:25 +0100

    feat: Added profiles viewset and authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,6 +10,9 @@ router = DefaultRouter()
 # to do that using the URL retrieving function providewd by dhango
 router.register("hello-viewset", views.HelloViewSet, base_name="hello-viewset")
 
+# we have the queryset in the views.py, so no need ot set base_name
+router.register("profile", views.UserProfileViewSet)
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path("", include(router.urls))
